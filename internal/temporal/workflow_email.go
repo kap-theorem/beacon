458125cb6db4c9e1c3a0ca198061adfdeb8e5cf2
Kushal Krishnappa <kushalkrishnappa333@gmail.com>
2025-11-15 23:02:03 -0500

    feat: added retry policy for

--- internal/temporal/workflow_email.go
+++ internal/temporal/workflow_email.go
@@ -4,11 +4,18 @@ import (
 	"beacon/internal/models"
 	"time"
 
+	"go.temporal.io/sdk/temporal"
 	"go.temporal.io/sdk/workflow"
 )
 
 func SendEmailWorkflow(ctx workflow.Context, msg *models.EmailMessage) error {
 	ctx = workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
+		RetryPolicy: &temporal.RetryPolicy{
+			InitialInterval:    time.Second * 5,
+			BackoffCoefficient: 2.0,
+			MaximumInterval:    time.Minute * 2,
+			MaximumAttempts:    3,
+		},
 		StartToCloseTimeout: time.Minute * 2,
 	})
 	return workflow.ExecuteActivity(ctx, "SendEmailActivity", msg).Get(ctx, nil)
